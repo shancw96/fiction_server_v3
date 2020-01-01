b82025111abad820e2f2f087f77f8197bd0509ef
shanCW <792878578@qq.com>
2020-01-01 22:31:30 +0800

    add static source

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ const cors = require("koa2-cors");
 const bodyParser = require("koa-bodyparser");
 const router = require("./router/root");
 
+//static source import
+const static = require("koa-static");
+const fs = require("fs");
+const path = require("path");
+const diskPath = "../oh-my-fiction-v3/oh-my-fiction-dist";
+
 const PORT = 4008;
 const server = new Koa();
 
 server.listen(PORT, () => {
-  console.log(`new ficiton server start at port:${PORT}`);
+    console.log(`new fiction server start at port:${PORT}`);
 });
 
+router.get(
+    "/",
+    ctx =>
+        (ctx.body = fs.readFileSync(
+            path.resolve(__dirname, "../oh-my-fiction-v3/oh-my-fiction-dist/index.html"),
+            "utf-8"
+        ))
+);
+
 server
-  .use(bodyParser())
-  .use(cors())
-  .use(router.routes())
-  .use(router.allowedMethods());
+    .use(bodyParser())
+    .use(cors())
+    .use(static(path.join(__dirname, diskPath)))
+    .use(router.routes())
+    .use(router.allowedMethods());
